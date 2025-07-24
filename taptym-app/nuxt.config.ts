b57bb0b9ea3a6d604b7eef54acb89b3985e1847a
imangali200 <imangalikazybek1975@gmail.com>
2025-07-24 16:23:11 +0500

    add docker

--- taptym-app/nuxt.config.ts
+++ taptym-app/nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+  nitro: {
+    output: {
+      dir: ".output",
+    },
+  },
 });
